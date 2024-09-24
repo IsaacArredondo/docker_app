4edb398df6bfb0c8c588362715335e84d829b332
Isaac Arredondo Padron <isaacarredondopadron@gmail.com>
2024-09-23 22:27:20 -0600

    fixing python code

--- app/main.py
+++ app/main.py
@@ -1,21 +1,22 @@
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
+import matplotlib.pyplot as plt  # Falta esta importación
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import mpl_toolkits.mplot3d  # Importación necesaria para proyecciones en 3D
 
 app = FastAPI()
 
 @app.get("/iris")
 def get_iris():
-    from sklearn import datasets
-
+    # Carga el conjunto de datos de Iris
     iris = datasets.load_iris()
-    # unused but required import for doing 3d projections with matplotlib < 3.2
-    import mpl_toolkits.mplot3d  # noqa: F401
 
-    from sklearn.decomposition import PCA
-
+    # Configura el gráfico en 3D
     fig = plt.figure(1, figsize=(8, 6))
     ax = fig.add_subplot(111, projection="3d", elev=-150, azim=110)
 
+    # Realiza la reducción de dimensionalidad con PCA
     X_reduced = PCA(n_components=3).fit_transform(iris.data)
     ax.scatter(
         X_reduced[:, 0],
@@ -25,6 +26,7 @@ def get_iris():
         s=40,
     )
 
+    # Configura los títulos y etiquetas
     ax.set_title("First three PCA dimensions")
     ax.set_xlabel("1st Eigenvector")
     ax.xaxis.set_ticklabels([])
@@ -33,6 +35,9 @@ def get_iris():
     ax.set_zlabel("3rd Eigenvector")
     ax.zaxis.set_ticklabels([])
 
+    # Guarda la imagen en un archivo PNG
     plt.savefig("iris.png")
+    
+    # Abre el archivo y devuelve la respuesta
     file = open("iris.png", "rb")
     return StreamingResponse(file, media_type="image/png")
